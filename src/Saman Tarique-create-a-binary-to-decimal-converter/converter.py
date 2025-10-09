39e0dbc14f8fbe638001923f189c43e85e816701
Saman Tariq <samantarique02@gmail.com>
2025-10-09 15:05:34 +0500

    Create converter.py
    Added a beginner-friendly Python project: Binary to Decimal Converter
    
    Features:
    - Converts binary string input to decimal value
    - Handles empty input, spaces, and invalid characters
    - Preserves leading zeros for accurate conversion
    - Fully functional with Python built-in functions (no external libraries)
    
    Example usage:
    Input: 001011 → Output: 11

---
+++ src/Saman Tarique-create-a-binary-to-decimal-converter/converter.py
@@ -0,0 +1,19 @@
+while True:
+    value=input("Enter binary value for conversion =").strip()
+    if value==""and value==" ":
+        print("Error! input cannot be empty:Please try again =")
+        continue
+    if not all (digit in "01" for digit in value):
+        print("Error only 0 and 1 are allowed")
+        continue
+    break
+
+
+result = 0
+power=0
+
+for i in reversed(str(value)):
+    result = result + (int(i) * 2**power)
+    power+=1
+    
+print(result)
